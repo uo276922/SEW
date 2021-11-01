ab67ef2c1dfc11c73f312c2b154ce97d1313dfa3
uo276922 <90771999+uo276922@users.noreply.github.com>
2021-11-01 15:34:47 +0100

    Delete Cabecera.js

--- Cabecera.js
+++
@@ -1,9 +0,0 @@
-// Cabecera.js
-var asignatura = new Object();
-asignatura.nombre = "Software y estándares en la Web";
-asignatura.titulacion = "Grado en Ingeniería Informática del Software";
-asignatura.centro = "Escuela de Ingeniería Informática";
-asignatura.universidad = "Universidad de Oviedo";
-asignatura.curso = "2021-2022";
-asignatura.alumno = "José Rodríguez Rico";
-asignatura.email = "UO276922@uniovi.es";
